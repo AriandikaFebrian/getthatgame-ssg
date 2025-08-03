cc24dea691a6a8133db5a71f69ec4f82937b5033
AriandikaFebrian <ariandika1913@gmail.com>
2025-08-03 16:31:43 +0700

    crouch: testing flexible

--- src/lib/shortlinks.ts
+++ src/lib/shortlinks.ts
@@ -2,7 +2,6 @@
 
 export const shortlinks: Record<string, Partial<Record<'shrinkme' | 'healthside' | 'linkvertise', string>>> = {
   'unlock-s-o-s-f-o-m-t-mega': {
-    shrinkme: 'https://shrinkme.ink/unlock-s-o-s-f-o-m-t-mega',
     healthside: 'https://link-center.net/1377708/7G6AEsT8opi2',
   },
 };

13bceba78b1e0c55cb6da78e1d170726b4ab203a
AriandikaFebrian <ariandika1913@gmail.com>
2025-08-03 17:20:50 +0700

    test: token unlock redirect basic working

--- src/lib/shortlinks.ts
+++ src/lib/shortlinks.ts
@@ -1,13 +1,9 @@
 // lib/shortlinks.ts
 
-export const shortlinks: Record<string, Partial<Record<'shrinkme' | 'healthside' | 'linkvertise', string>>> = {
+export const shortlinks: Record<string, Partial<Record<'shrinkme' | 'linkvertise', string>>> = {
   'unlock-s-o-s-f-o-m-t-mega': {
-    shrinkme: 'https://shrinkme.io/unlock-rdr2-mega',
-    linkvertise: 'https://healthside.link/unlock-rdr2-mega',
-  },
-  'unlock-gta-s-a-d-e-mega': {
-    shrinkme: 'https://shrinkme.io/unlock-rdr2-mega',
-    linkvertise: 'https://healthside.link/unlock-rdr2-mega',
+    shrinkme: '',
+    linkvertise: 'https://link-center.net/1377708/7G6AEsT8opi2',
   },
 
 };
